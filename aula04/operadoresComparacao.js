394a8504732781a9986daef5bea1cc9454e1c621
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-07-25 10:54:34 -0300

    Aula 3 e aula 4

--- aula04/operadoresComparacao.js
+++ aula04/operadoresComparacao.js
@@ -1,8 +1,13 @@
 let n1 = "20";
 let n2 = 20;
 
-// let verificador = n1 == n2;
+let verificador = n1 == n2;
 // let verificador = n1 === n2;
-let verificador = n1 != n2;
+// let verificador = n1 != n2;
+// let verificador = n1 !== n2;
+// let verificador = n1 < n2;
+// let verificador = n1 > n2;
+// let verificador = n1 <= n2;
+// let verificador = n1 >= n2;
 
 console.log(verificador);
